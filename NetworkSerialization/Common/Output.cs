8cd854626cc43b5e6636fc3f0fa829260bafce26
ilyaDubovtsev <dubovtsev98@gmail.com>
2019-11-23 19:49:30 +0400

    + Serialization lab

---
+++ NetworkSerialization/Common/Output.cs
@@ -0,0 +1,9 @@
+namespace Common
+{
+    public class Output
+    {
+        public decimal SumResult { get; set; }
+        public int MulResult { get; set; }
+        public decimal[] SortedInputs { get; set; }
+    }
+}
---
+++ NetworkSerialization/Lab1/LabSerialization.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Common;
+using Serialization;
+
+namespace Lab1
+{
+    public class LabSerialization
+    {
+        public static string Process(string serializationTypeString, string serializedInputString)
+        {
+            var serializationType = StringToSerializationType(serializationTypeString);
+            var serializationProvider = SuperSerializer.GetSerializer(serializationType);
+
+            var input = serializationProvider.Deserialize<Input>(serializedInputString);
+
+            var output = new Output()
+            {
+                SumResult = input.Sums.Sum(s => s * input.K),
+                MulResult = input.Muls.Aggregate(1, (current, mul) => current * mul),
+                SortedInputs = input.Sums
+                    .Concat(input.Muls.Select(x => (decimal) x))
+                    .OrderBy(x => x)
+                    .ToArray()
+            };
+
+            var serializedOutput = serializationProvider.Serialize(output);
+
+            return serializedOutput;
+        }
+
+        private static SerializationType StringToSerializationType(string serializationTypeString)
+        {
+            switch (serializationTypeString.ToLower())
+            {
+                case "json":
+                    return SerializationType.Json;
+                case "xml":
+                    return SerializationType.Xml;
+                default:
+                    throw new ArgumentException($"Неопознанный тип сериализации {serializationTypeString}");
+            }
+        }
+    }
+}
