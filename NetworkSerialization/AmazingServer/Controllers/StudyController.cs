71d154e1b295367afd8ee9e82cb5bba7ffb8beba
ilyaDubovtsev <dubovtsev98@gmail.com>
2019-11-26 00:09:45 +0400

    * add PostInputData

--- NetworkSerialization/AmazingServer/Controllers/StudyController.cs
+++ NetworkSerialization/AmazingServer/Controllers/StudyController.cs
@@ -1,3 +1,4 @@
+using Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AmazingServer.Controllers
@@ -6,11 +7,25 @@ namespace AmazingServer.Controllers
     [ApiController]
     public class StudyController : ControllerBase
     {
-        // GET study/ping
+        // GET Study/Ping
         [HttpGet("ping")]
         public ActionResult Ping()
         {
             return Ok();
+        }
+
+        // GET Study/PostInputData
+        [HttpGet("PostInputData")]
+        public JsonResult PostInputData()
+        {
+            var input = new Input
+            {
+                K = 10,
+                Sums = new[] {1.01m, 2.02m},
+                Muls = new[] {1, 4}
+            };
+
+            return new JsonResult(input);
         }
     }
 }
